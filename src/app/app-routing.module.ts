b5ec857516ba7e118e8a5a35793d895b86ce41d5
The Buildbot <buildbot@angular2buch.de>
2018-12-10 23:03:55 +0000

    Auto-generated commit --
    Triggered by commit: https://github.com/angular-schule/book-monkey3-src/commit/2a5bb7db573f2432d3b32d9b330a22dc3e83a141
    CircleCI build: https://circleci.com/gh/angular-schule/book-monkey3-src/181

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,33 +1,10 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
-import { HomeComponent } from './home/home.component';
-import { CanNavigateToAdminGuard } from './can-navigate-to-admin.guard';
-
-const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'home',
-    component: HomeComponent
-  },
-  {
-    path: 'books',
-    loadChildren: 'src/app/books/books.module#BooksModule'
-  },
-  {
-    path: 'admin',
-    loadChildren: 'src/app/admin/admin.module#AdminModule',
-    canActivate: [CanNavigateToAdminGuard]
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-  providers: [CanNavigateToAdminGuard]
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
